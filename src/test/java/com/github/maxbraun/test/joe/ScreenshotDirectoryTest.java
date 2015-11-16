a0e9f2e56a07aa40201926f02386f7beb6af5235
the-matti <online@der-matti.de>
2015-11-16 19:04:19 +0100

    - parametrized screenshot-naming      - directory structure based on executed tests      - added additional events where screenshots are taken      - disabled maven-enforcer-plugin

--- src/test/java/com/github/maxbraun/test/joe/ScreenshotDirectoryTest.java
+++ src/test/java/com/github/maxbraun/test/joe/ScreenshotDirectoryTest.java
@@ -1,7 +1,19 @@
 package com.github.maxbraun.test.joe;
 
-/**
- * Created by mab on 11/16/15.
- */
+import net.oneandone.sushi.fs.Node;
+import net.oneandone.sushi.fs.World;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ScreenshotDirectoryTest {
+
+    @Test
+    public void testDirectoryNaming() throws Exception {
+        ScreenshotDirectory screenshotDirectory = new ScreenshotDirectory(this.getClass(), new World());
+        Node node = screenshotDirectory.methodDirectory("method[foo|bar]");
+        assertEquals("bar", node.getName());
+        assertEquals("foo", node.getParent().getName());
+        assertEquals("method", node.getParent().getParent().getName());
+    }
 }
